ec68d232f7121a4e50288f1b57efc2c0e9aba7bf
Marshall <marshallythd@gmail.com>
2024-07-28 20:26:03 -0400

    Update and rename entrypoint.sh to start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Replace placeholders in config.toml.sample with environment variable values
+envsubst < /data/config.toml.sample > /data/config.toml
+
+# Execute the Python script
+exec python bot.py
