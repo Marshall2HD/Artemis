bf07af5b52d3033d5241ebfda9c830c593b8889b
Marshall <marshallythd@gmail.com>
2024-07-28 20:09:31 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-# entrypoint.sh
 
-# Use envsubst to substitute environment variables into config.toml
+# Generate config.toml from the template using environment variables
 if [ ! -f /data/config.toml ]; then
     echo "Generating config.toml from the template..."
     envsubst < /data/config.toml.sample > /data/config.toml
 fi
 
-# Start the main application
+# Execute the main application
 exec python /app/bot.py
